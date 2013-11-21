e53500ce2e6585a2c9c34910bdb22059a9174aa8
GlorymarGarcia <Audrey@GG.local>
2013-11-21 18:44:00 +0800

    Listing of a post responses (*needs improvement).

---
+++ ReliefBoard/src/com/stratpoint/reliefboard/adapter/ResponsesAdapter.java
@@ -0,0 +1,53 @@
+package com.stratpoint.reliefboard.adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.stratpoint.reliefboard.model.ResponseHelp;
+import com.stratpoint.reliefboardandroid.R;
+
+public class ResponsesAdapter extends ArrayAdapter<ResponseHelp> {
+	private int mLayoutResourceId;
+	private List<ResponseHelp> mPosts;
+	private LayoutInflater mInflater;
+
+	public ResponsesAdapter(Context context, int layoutResourceId, List<ResponseHelp> posts) {
+		super(context, layoutResourceId);
+		mLayoutResourceId = layoutResourceId;
+		posts = mPosts;
+		mInflater = LayoutInflater.from(context);
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent){
+		View view = convertView;
+		ViewHolder viewHolder = null;
+		ResponseHelp post = mPosts.get(position);
+		
+		if(view == null){
+			view = mInflater.inflate(mLayoutResourceId, null);
+			viewHolder = new ViewHolder();
+			viewHolder.user_name = (TextView) view.findViewById(R.id.user_name);
+			viewHolder.user_response = (TextView) view.findViewById(R.id.user_comment);
+			view.setTag(viewHolder);
+		} else {
+			viewHolder = (ViewHolder) view.getTag();
+		}
+		
+		viewHolder.user_name.setText(post.getMessage());
+		
+		return view;
+	}
+	
+	private class ViewHolder {
+		public TextView user_name;
+		public TextView user_response;
+	}
+	
+}
